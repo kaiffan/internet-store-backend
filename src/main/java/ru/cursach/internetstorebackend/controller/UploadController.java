2e1c63376da9a277e72bc8590c6a9d1624785842
ErKoMi <konst_ermakov03@mail.ru>
2023-12-15 02:32:43 +0300

    Добавил контроллер для загрузки файлов на сервер

---
+++ src/main/java/ru/cursach/internetstorebackend/controller/UploadController.java
@@ -0,0 +1,37 @@
+package ru.cursach.internetstorebackend.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ru.cursach.internetstorebackend.services.FileService;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/upload")
+public class UploadController {
+
+    private FileService fileService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> handleFileUpload(@RequestParam("name") String name,
+                                   @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                String uploadedPath = fileService.uploadFile(name, file);
+                return new ResponseEntity<>(uploadedPath, HttpStatus.OK);
+            } catch (IOException e) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ src/main/java/ru/cursach/internetstorebackend/services/FileServiceWebDavImpl.java
@@ -0,0 +1,23 @@
+package ru.cursach.internetstorebackend.services;
+
+import com.github.sardine.Sardine;
+import com.github.sardine.SardineFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class FileServiceWebDavImpl implements FileService {
+
+    String fileServerUrl = "http://25.33.65.57:5050/product_images/";
+
+    @Override
+    @Async
+    public String uploadFile(String name, MultipartFile file) throws IOException {
+        Sardine sardine = SardineFactory.begin();
+        sardine.put(fileServerUrl + name, file.getInputStream());
+        return fileServerUrl + name;
+    }
+}
